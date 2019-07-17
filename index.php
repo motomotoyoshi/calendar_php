944bce7f10f423d2515c05e956c53e0a0bb0b995
motomotoyoshi <motomoto.git@gmail.com>
2019-07-17 15:26:11 +0000

    update, index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Calender</title>
+</head>
+
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th><a href="">&laquo;</a></th>
+        <th colspan="5">July 2019</th>
+        <th><a href="">&raquo;</a></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Sun</td>
+        <td>Mon</td>
+        <td>Tue</td>
+        <td>Wed</td>
+        <td>Thu</td>
+        <td>Fri</td>
+        <td>Sat</td>
+      </tr>
+      <tr>
+        <td class="youbi_0">1</td>
+        <td class="youbi_1">2</td>
+        <td class="youbi_2">3</td>
+        <td class="youbi_3">4</td>
+        <td class="youbi_4 today">5</td>
+        <td class="youbi_5">6</td>
+        <td class="youbi_6">7</td>
+      </tr>
+      <tr>
+        <td class="youbi_0">30</td>
+        <td class="youbi_1">31</td>
+        <td class="gray">1</td>
+        <td class="gray">2</td>
+        <td class="gray">3</td>
+        <td class="gray">4</td>
+        <td class="gray">5</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <th colspan="7"><a href="">Today</a></th>
+    </tfoot>
+  </table>
+</body>
+
+</html>
